273bd306c51d1212c061095f234e2aa92114fd5a
ebin58 <sebastianebin83@gmail.com>
2025-03-25 12:26:52 -0400

    Completed training in gesture_new_dataset_to_train.
    did 20 epochs.
    
    results saved under top level run folder under custom_yolo_model5

---
+++ gesture_new_dataset_to_train/training.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+
+# # Load a model
+# model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+
+# # Train the model
+# results = model.train(data="C:/Users/ebin5/ENEE408I_Files/2025_Spring_Team_3/gesture_new_dataset_to_train/data.yaml", epochs=20, imgsz=640, name='custom_yolo_model', device='cuda')
+
+def train_model():
+    model = YOLO("yolo11n.pt")  # Load the YOLO model
+
+    # Train the model on GPU
+    results = model.train(
+        data="C:/Users/ebin5/ENEE408I_Files/2025_Spring_Team_3/gesture_new_dataset_to_train/data.yaml",
+        epochs=20, imgsz=640,
+        name='custom_yolo_model',
+        device='cuda'  # Run on GPU
+    )
+
+    print("Training complete. Check the weights folder for the trained model.")
+    model.save("simonsaysv1.pt")
+
+if __name__ == '__main__':
+    train_model()
